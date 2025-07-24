8fae6039b41481864d1bfa7d714329d735611bb6
NanaQuaci <aducollins49@gmail.com>
2025-07-24 14:33:09 +0000

    Submission on individual UI Automation assessment

---
+++ src/test/java/com/example/base/BaseTest.java
@@ -0,0 +1,46 @@
+package com.example.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+    protected final String BASE_URL = "https://www.saucedemo.com/";
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--incognito");
+        // options.addArguments("--headless=new");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
